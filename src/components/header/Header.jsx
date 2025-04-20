1cdbc1158eb03f0c838e05f87ad0274ac689f0bd
Alberto <dany.canaviri2022@gmail.com>
2025-04-19 23:58:32 -0400

    created Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import Logo from '../../assets/logo.png'
+import { navLinks } from '../../Data'
+import ScrollLink from '../link/ScrollLink'
 
 const header = () => {
-  return (
-    <div>header</div>
-  )
+	return (
+
+		<header className="header">
+
+			<nav className="nav container">
+
+				<a href="/" className="nav-logo">
+				
+					<img src={ Logo } alt="" className="nav-logo-img"/>
+
+				</a>
+
+				<div className="nav-menu">
+
+					<ul className="nav-list">
+
+						{navLinks.map((navLink, index) => {
+
+							return (
+
+								<li className="nav-item" key={index}>
+
+									<ScrollLink to={navLink} name={navLink} className="nav-link"/>
+
+								</li>
+
+							);
+
+						})}
+
+					</ul>
+
+				</div>
+
+			</nav>
+
+		</header>
+
+	)
 }
 
 export default header
---
+++ src/components/link/ScrollLink.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ScrollLink = ({to, name, className}) => {
+
+	return (
+
+		<a href={to} className={{className}}>
+
+			{{name}}
+			
+		</a>
+
+	)
+
+}
+
+export default ScrollLink
